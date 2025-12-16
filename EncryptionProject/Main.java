bcca17760ee206c9edc05d66e8ffbb1063867849
JenniferW21 <jenniferw57@nycstudents.net>
2025-12-16 01:31:24 +0000

    java

--- EncryptionProject/Main.java
+++ EncryptionProject/Main.java
@@ -5,6 +5,7 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
 
+
   void init(){
    
     // Array1: Specific characters
@@ -19,6 +20,8 @@ class Main {
  
 
 
+
+
     // Array2: Unicode characters
     char[] braillePattern = new char[7]; //sub replace with sub2
       braillePattern[0] = '\u2803';  // BRAILLE PATTERN DOTS-12(b)
@@ -29,7 +32,8 @@ class Main {
       braillePattern[5] = '\u2811';  // BRAILLE PATTERN DOTS-15(e)
       braillePattern[6] = '\u2832';  // BRAILLE PATTERN DOTS-256(.)
 
-    
+
+   
     // Encoding the original text:
     String file1 = Input.readFile("Original.txt");
     // Encode level 1 (String Manipulation - Reverse)
@@ -43,23 +47,26 @@ class Main {
     String encodedMsg3 = roundRobin1(encodedMsg2,roundRobin1);
     Input.writeFile("Encode3.txt", encodedMsg3);
 
-    
-    // // Decoding the encoded text: 
-    // String file2 = Input.readFile("Encode3.txt");
-    // // Decode level 1  (Cipher elevated - round robbin shift)
-    // int [] roundRobin2 = {4,3,2,1};
-    // String decodedMsg1 = roundRobin2(file2,roundRobin2);
-    // Input.writeFile("Decode1.txt", decodedMsg1);
-    // Decode level 2 (Substitution)
-    // String decodedMsg2 = subEncryption(decodedMsg1, braillePattern, letter);
-    // Input.writeFile("Decode2.txt", decodedMsg2);
-    // // Decode level 3 (String Manipulation - ReverseBack)
-    // String decodedMsg3 = reverse(decodedMsg2);
-    // Input.writeFile("Decode1.txt", decodedMsg3);
 
-    
-    
+   
+    // Decoding the encoded text:
+    String file2 = Input.readFile("Encode3.txt");
+    // Decode level 1  (Cipher elevated - round robbin shift)
+    int [] roundRobin2 = {1,2,3,4};
+    String decodedMsg1 = roundRobin2(file2,roundRobin2);
+    Input.writeFile("Decode1.txt", decodedMsg1);
+    // Decode level 2 (Substitution)
+    String decodedMsg2 = subEncryption(decodedMsg1, braillePattern, letter);
+    Input.writeFile("Decode2.txt", decodedMsg2);
+    // Decode level 3 (String Manipulation - ReverseBack)
+    String decodedMsg3 = reverse(decodedMsg2);
+    Input.writeFile("Decode3.txt", decodedMsg3);
+
+
+   
+   
   }
+
 
   // reverse a string (encode 1)
   String reverse(String msg){
@@ -69,13 +76,6 @@ class Main {
     }
     return build;
   }
-  // String reverse(String s){
-  //   String build = ""; // building a string
-  //   for(int x=0; x<=s.length()-2; x+=2){ // for loop
-  //       build += s.substring(x+1,x+2) + s.substring(x,x+1); // Switching letter
-  //   }
-  //   return build;
-  // }
 
   // Substitution (encode2)
   String subEncryption(String msg1, char[] letter, char[] bRaille){
@@ -96,25 +96,47 @@ class Main {
   }
 
 
-  // Round Robbin +1 encoding (encode 3)
+
+
+  // Round Robbin encoding (encode 3)
   String roundRobin1(String msg2, int[] rRobinshift){ //(encodedmsg1, the array of shifts)
     String build = "";
     int rRobin = 0;
     int ascii = 0;
     char ch = '\0';
-    
+   
     for(int x=0; x < msg2.length(); x++){//forloop to go through each letter of the string
       ch = msg2.charAt(x); //position of each character
       ascii = (int)ch; //takes each char and cast into integer
       ascii += rRobinshift[rRobin]; // array[0] position 0 would be shift of 1
-      build += (char)ascii; //convert to character 
+      build += (char)ascii; //convert to character
       rRobin++; //the array position goes up
-      if(rRobin == 4){ //4 shifts in total{1,2,3,4} after goes back to position 0 
+      if(rRobin == 4){ //4 shifts in total{1,2,3,4} after goes back to position 0
         rRobin = 0;             //position(0,1,2,3)
       }  
     }
     return build;
   }
+
+// Round Robbin decoding (decode 1)
+  String roundRobin2(String msg3, int[] rRobinshift){ //(encodedmsg1, the array of shifts)
+    String build = "";
+    int rRobin = 0;
+    int ascii = 0;
+    char ch = '\0';
+   
+    for(int x=0; x < msg3.length(); x++){//forloop to go through each letter of the string
+      ch = msg3.charAt(x); //position of each character
+      ascii = (int)ch; //takes each char and cast into integer
+      ascii -= rRobinshift[rRobin]; // array[0] position 0 would be shift of 1
+      build += (char)ascii; //convert to character
+      rRobin++; //the array position goes up
+      if(rRobin == 4){ //4 shifts in total{1,2,3,4} after goes back to position 0
+        rRobin = 0;             //position(0,1,2,3)
+      }  
+    }
+    return build;
+  } 
 
 
   // identifying index of char within array
@@ -127,10 +149,14 @@ class Main {
     return -1;
   }
 
+
   // random integer generator
   int randInt(int lower, int upper){
     int range = upper - lower + 1;
     return (int)(Math.random()*range) + lower;
   }
 
+
 }
+
+
